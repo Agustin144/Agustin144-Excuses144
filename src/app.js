fcfa8f47d1a8203e7853587e616463fb37df06f5
Agustin Rolando <138535452+Agustin144@users.noreply.github.com>
2023-07-12 17:03:31 -0300

    hola

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [ "before the class", "tight on time","when I finished","during my lunch", "while I was praying"];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+
+  const aleatorio_persona =  who[Math.floor(Math.random() * who.length)]
+  const aleatorio_action =  action[Math.floor(Math.random() * action.length)]
+  const aleatorio_what =  what[Math.floor(Math.random() * what.length)]
+  const aleatorio_when=  when[Math.floor(Math.random() * when.length)]
+  const frse= aleatorio_persona+" "+aleatorio_action+" "+ aleatorio_what+" "+ aleatorio_when
+  console.log(aleatorio_persona, aleatorio_action, aleatorio_what, aleatorio_when);
+document.getElementById("frase").innerHTML=`${frse}`
+  };
